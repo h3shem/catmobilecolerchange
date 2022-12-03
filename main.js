8e27c0f18dc0c4b3a494595987fdd30e71ab4cfb
h3shem <h3shem@gmail.com>
2022-12-04 00:11:31 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let imgsliderhome = document.querySelector(".siliderimgehome");
+let contener = document.querySelector(".contener");
+
+function phoneschange(phone){
+    imgsliderhome.src = phone;
+}
+function coloerchange(color){
+    contener.style.background=color;
+
+}
